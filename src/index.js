29711b9e463dc5189e22c369aecfa0178122c071
Ruan Pablo <ruan_goio10@hotmail.com>
2022-05-13 19:52:12 -0300

    Params types

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 const express = require('express');
+
 const app = express();
+app.use(express.json());
 
 app.listen(3333, () => {
     console.log('Server open on port 3333 🔥');
 });
 
 app.get('/courses', (req, res) => {
+    const query = req.query;
+    console.log(query);
     return res.json(["curso1", "curso2", "curso3"]);
 });
 
 app.post('/courses', (req, res) => {
+    const body = req.body;
+    console.log(body);
     return res.json(["curso 1", "curso 2", "curso 3", "curso 4"]);
 });
 
 app.put("/courses/:id", (req, res) => {
+    const params = req.params;
+    const { id } = params;
+    console.log(id);
     return res.json(["curso 6", "curso 2", "curso 3", "curso 4"]);
 })
 
